63e5e317ddd0fc2b96a375bb1dbd0327ca9049cd
itsahmedkhalil <itsahmedkhalil@gmail.com>
2021-09-27 19:16:38 -0500

    added magnetometer sensor data

--- Position_Tracking/arduino/accessing_IMU/accessing_IMU.ino
+++ Position_Tracking/arduino/accessing_IMU/accessing_IMU.ino
@@ -9,6 +9,7 @@ This example reads the acceleration values as relative direction and degrees,5  
 
 float Ax, Ay, Az;
 float Gx, Gy, Gz;
+float Mx, My, Mz;
 
 void setup() {
   Serial.begin(9600);
@@ -24,12 +25,24 @@ void setup() {
   Serial.print(IMU.accelerationSampleRate());  
   Serial.print(',');
   //Serial.print("Hz"); 
-
+  
   //Serial.print("Gyroscope sample rate = ");  
   Serial.print(IMU.gyroscopeSampleRate());
+  Serial.print(',');
   //Serial.print("Hz");
-
+  
+  // note the FS value does not change the output of the read method, it just assigns more bits to the chip output,
+  // increasing accuracy at the cost of a smaller range 
+  IMU.setMagnetFS(0); //   1=±800µT   2=±1200µT  3=±1600µT  Default= 0:±400 µT 
+  IMU.magnetUnit = MICROTESLA;  //   GAUSS   MICROTESLA   NANOTESLA
+  
+  // Change the sample frequency ( ODR = Output Dats rate)  
+  // Note: setting 0..5 did not work on all systems. The default setting = 5 for compatibility reasons 
+  IMU.setMagnetODR(7);   // Sampling rate (0..8)->{0.625,1.25,2.5,5.0,10,20,40,80,400}Hz  
+  Serial.print(IMU.getMagnetODR());                       // alias IMU.magneticFieldSampleRate());
   Serial.println();
+  //Serial.println(" Hz");
+  
 
 }
 
